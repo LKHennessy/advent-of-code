46dddae11be5f52b3c6cc81d908f05d23d2d0c67
Lauren Hennessy <lauren.hennessy.work@gmail.com>
2017-12-11 14:42:52 +1100

    Complete solution for spiral memory

--- 2017/day-3/spiral_memory.rb
+++ 2017/day-3/spiral_memory.rb
@@ -6,6 +6,51 @@ class SpiralMemory
   end
 
   def self.create_data(input)
-    {1 => [0,0], 2 => [1,0], 3 => [1,1]}
+    data = {1 => [0,0]}
+    direction = :right
+    current_number = 2
+    steps_to_next_turn = 1
+
+    while current_number <= input
+      2.times do
+        case direction
+          when :right
+            steps_to_next_turn.times do
+              x = data[current_number - 1][0]
+              y = data[current_number - 1][1]
+              data[current_number] = [x + 1, y]
+              current_number += 1
+              direction = :up
+            end
+          when :up
+            steps_to_next_turn.times do
+              x = data[current_number - 1][0]
+              y = data[current_number - 1][1]
+              data[current_number] = [x, y + 1]
+              current_number += 1
+              direction = :left
+            end
+          when :left
+            steps_to_next_turn.times do
+              x = data[current_number - 1][0]
+              y = data[current_number - 1][1]
+              data[current_number] = [x - 1, y]
+              current_number += 1
+              direction = :down
+            end
+          when :down
+            steps_to_next_turn.times do
+              x = data[current_number - 1][0]
+              y = data[current_number - 1][1]
+              data[current_number] = [x, y - 1]
+              current_number += 1
+              direction = :right
+            end
+        end
+      end
+      steps_to_next_turn += 1
+    end
+
+    data
   end
 end
--- 2017/day-3/spiral_memory_test.rb
+++ 2017/day-3/spiral_memory_test.rb
@@ -11,17 +11,18 @@ class SpiralMemoryTest < MiniTest::Test
   end
 
   def test_distance_3
-    skip
     assert_equal 3, SpiralMemory.distance(12)
   end
 
   def test_distance_4
-    skip
     assert_equal 2, SpiralMemory.distance(23)
   end
 
   def test_distance_5
-    skip
     assert_equal 31, SpiralMemory.distance(1024)
+  end
+
+  def test_distance_6
+    assert_equal 430, SpiralMemory.distance(312051)
   end
 end
