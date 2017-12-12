13fc85f35cfa609099d04d24bda9127585ce4ef3
Lauren Hennessy <lauren.hennessy.work@gmail.com>
2017-12-12 17:52:03 +1100

    complete solution for jump maze part two

---
+++ 2017/day-5/jump_maze_part_two.rb
@@ -0,0 +1,17 @@
+class JumpMazePartTwo
+  def self.jump(maze)
+    maze = maze.each_line.map { |offset| offset.split.map(&:to_i) }.flatten
+
+    current_index = 0
+    steps = 0
+
+    while maze[current_index]
+      current_argument = maze[current_index]
+      steps += 1
+      current_argument < 3 ? maze[current_index] += 1 : maze[current_index] -= 1
+      current_index = current_index + current_argument
+    end
+
+    steps
+  end
+end
